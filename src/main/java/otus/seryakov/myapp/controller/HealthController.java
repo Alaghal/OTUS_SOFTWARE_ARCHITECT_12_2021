816fd4498901a2509ccd8cecf847ffe3483119df
Alaghal <seryakov.andreyv@gmail.com>
2021-01-31 13:51:42 +0700

    Init commit spring initializer

---
+++ src/main/java/otus/seryakov/myapp/controller/HealthController.java
@@ -0,0 +1,15 @@
+package otus.seryakov.myapp.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public ResponseEntity getHealth(){
+        return ResponseEntity.ok().build();
+    }
+
+}
---
+++ src/test/java/otus/seryakov/myapp/MyappApplicationTests.java
@@ -0,0 +1,13 @@
+package otus.seryakov.myapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
